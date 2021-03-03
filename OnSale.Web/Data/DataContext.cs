b3ff04dcca4cd2f85511d078649e5a3c2d1d565f
Marco Antonio Luna Calvillo <mlunac08@outlook.com>
2021-03-02 18:46:58 -0600

    15 - Adicionando Tablas de Pedidos

--- OnSale.Web/Data/DataContext.cs
+++ OnSale.Web/Data/DataContext.cs
@@ -16,6 +16,9 @@ namespace OnSale.Web.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
